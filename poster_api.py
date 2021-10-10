cd53ceb61ad924613aeb091a671f100983c95a38
domaskal <domaskal@gmail.com>
2021-10-10 20:09:18 +0300

    Create with permissions and properly find show name if filename contains "Season"

--- poster_api.py
+++ poster_api.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 MOVIE_DIR=os.environ.get('MOVIE_DIR', None)
 MOVIE_POSTER_DIR=os.environ.get('MOVIE_POSTER_DIR', None)
 TV_DIR=os.environ.get('TV_DIR', None)
-TV_POSTER_DIR=os.environ.get('TV_POSTER_DIR')
+TV_POSTER_DIR=os.environ.get('TV_POSTER_DIR', None)
 
 @app.route('/upload', methods=['POST'])
 def upload_poster():
@@ -23,20 +23,23 @@ def upload_poster():
     name = parameters['name'] # Name (Year)
     media_type = parameters['type'] # Movie or Show
 
-    # TODO: Determine all characters and how they are modified in radarr / sonarr
-    name = name.translate({ord(c): None for c in '!?@#$:-'})
-    name = name.translate({ord(c): '+' for c in '/'})
-
     if "/login" in url:
         app.logger.critical("You have to be logged in to download!");
         return '', 500
+
+    # TODO: Determine all characters and how they are modified in radarr / sonarr
+    name = name.translate({ord(c): None for c in '!?@#$:-'})
+    name = name.translate({ord(c): '+' for c in '/'})
+    name = (name.split(")")[0]) + ")"
 
     app.logger.info("URL: " + url)
     app.logger.info("Name: " + name)
     app.logger.info("Type: " + media_type)
 
     # Determine if directories for current type are set
+    download_directory = ""
     if media_type == "Movie":
+        download_directory = MOVIE_POSTER_DIR
         if MOVIE_DIR is None:
             app.logger.critical("MOVIE_DIR isn't set!")
             return '', 500
@@ -44,6 +47,7 @@ def upload_poster():
             app.logger.critical("MOVIE_POSTER_DIR isn't set!")
             return '', 500
     elif media_type == "Show":
+        download_directory = TV_POSTER_DIR
         if TV_DIR is None:
             app.logger.critical("TV_DIR isn't set!")
             return '', 500
@@ -51,7 +55,6 @@ def upload_poster():
             app.logger.critical("TV_POSTER_DIR isn't set!")
             return '', 500
 
-    download_directory = MOVIE_POSTER_DIR if media_type == "Movie" else TV_POSTER_DIR
     download_directory = os.path.join(download_directory, name)
     if not os.path.isdir(download_directory):
         os.makedirs(download_directory, 0o777)
@@ -66,7 +69,7 @@ def upload_poster():
         app.logger.critical("Failed to get poster!");
         return '', 500
 
-    with open(os.path.join(download_directory, filename), 'wb') as f: # open the file to write as binary - replace 'wb' with 'w' for text files
+    with open(os.open(os.path.join(download_directory, filename), os.O_CREAT | os.O_WRONLY, 0o777), 'wb') as f: # open the file to write as binary - replace 'wb' with 'w' for text files
         for chunk in r.iter_content(1024): # iterate on stream using 1KB packets
             f.write(chunk) # write the file
 
